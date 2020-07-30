54022dfed0b274793ae749522f508e9b5b243cc8
mrtajik <satymov@outlook.com>
2020-07-30 15:11:44 -0400

    Data Converting

---
+++ 1.py
@@ -0,0 +1,22 @@
+"""
+Data Converting
+"""
+#example the result would be list
+"""
+my_data="Hello,my,is,Mubin"
+my_data.split(",")
+['Hello', 'my', 'is', 'Mubin']
+"""
+
+#string to numbers and vice versa
+"""
+"Today is the "+str(28)
+'Today is the 28'
+"""
+
+#str to list
+"""
+list(str("Hello"))
+['H', 'e', 'l', 'l', 'o']
+"""
+
